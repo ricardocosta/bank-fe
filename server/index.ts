ded1a4074b37496bd46228a7a0231edc374a07a7
Ricardo Costa <ricardocosta1989@gmail.com>
2024-04-21 23:17:29 +0100

    feat: Remove usage of Permanent Redirect (301)

--- server/index.ts
+++ server/index.ts
@@ -51,7 +51,7 @@ app.get("*", (req, res, next) => {
   if (req.path.endsWith("/") && req.path.length > 1) {
     const query = req.url.slice(req.path.length);
     const safepath = req.path.slice(0, -1).replace(/\/+/g, "/");
-    res.redirect(301, safepath + query);
+    res.redirect(302, safepath + query);
   } else {
     next();
   }
