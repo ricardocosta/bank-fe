32f102e4d22e789237101346e560e7b27e9d6a8e
Ricardo Costa <ricardocosta1989@gmail.com>
2024-04-26 23:55:23 +0100

    feat: Remove unused MSW ping handler

--- tests/mocks/index.ts
+++ tests/mocks/index.ts
@@ -1,16 +1,9 @@
 import closeWithGrace from "close-with-grace";
-import { http, passthrough } from "msw";
 import { setupServer } from "msw/node";
 
 import { handlers as resendHandlers } from "./resend.ts";
 
-const miscHandlers = [
-  process.env.REMIX_DEV_ORIGIN
-    ? http.post(`${process.env.REMIX_DEV_ORIGIN}ping`, passthrough)
-    : null,
-].filter(Boolean);
-
-export const server = setupServer(...miscHandlers, ...resendHandlers);
+export const server = setupServer(...resendHandlers);
 
 server.listen({ onUnhandledRequest: "warn" });
 
