389b0e851582a82732d0ac39e205a0b659e31ae9
Ricardo Costa <commits@ricardocosta.dev>
2024-11-09 16:12:41 +0000

    feat: Add Heading component

---
+++ app/components/ui/typography/heading.tsx
@@ -0,0 +1,46 @@
+import { cva } from "class-variance-authority";
+import { forwardRef } from "react";
+
+import { cn } from "~/styles/classname.ts";
+
+import type { VariantProps } from "class-variance-authority";
+import type { HTMLAttributes } from "react";
+
+const headingStyles = {
+  h1: "text-h1",
+  h2: "text-h2",
+  h3: "text-h3",
+  h4: "text-h4",
+  h5: "text-h5",
+  h6: "text-h6",
+};
+
+const headingVariants = cva("font-heading font-bold", {
+  defaultVariants: {
+    variant: "h1",
+  },
+  variants: {
+    variant: headingStyles,
+  },
+});
+
+export type HeadingProps = HTMLAttributes<HTMLHeadingElement> &
+  VariantProps<typeof headingVariants> & {
+    level?: keyof typeof headingStyles;
+  };
+
+export const Heading = forwardRef<HTMLHeadingElement, HeadingProps>(
+  ({ className, variant, level = "h1", ...props }, ref) => {
+    const Component = level;
+
+    return (
+      <Component
+        className={cn(headingVariants({ className, variant }))}
+        {...props}
+        ref={ref}
+      />
+    );
+  },
+);
+
+Heading.displayName = "Heading";
