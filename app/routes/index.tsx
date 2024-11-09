b5fabf2821f95d42ce90f06d7d91193d40aa6fe3
Ricardo Costa <commits@ricardocosta.dev>
2024-11-09 16:26:15 +0000

    chore: Add showcase page

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -4,7 +4,7 @@ export const meta: MetaFunction = () => [{ title: "Epic Notes" }];
 
 export default function Index() {
   return (
-    <main className="font-heading flex w-full items-center justify-center">
+    <main className="flex w-full items-center justify-center font-heading">
       <p>Landing page</p>
     </main>
   );
